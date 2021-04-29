567c503a09dafcfaf282a5e5a085cbfb0f49ed5e
Veronika Ozretic <vozret00@fesb.hr>
2021-04-29 14:38:00 +0200

    making items removable (145, 146)

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,9 +12,13 @@ const Cart = (props) => {
 
   const hasItems = cartContext.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartContext.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartContext.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
